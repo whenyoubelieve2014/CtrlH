8627fbac5c467fb52f5abd8efd64d05186faf989
whenyoubelieve2014 <whenyoubelieve2014@gmail.com>
2014-02-09 23:05:19 -0500

    accordion + angularjs = not working

---
+++ jQuery/scripts/index/app.js
@@ -0,0 +1,58 @@
+﻿(function() {
+    var myApp = angular.module('app', []);
+
+    myApp.directive('accordionSortable', function () {
+        return {
+            // Restrict it to be an attribute in this case
+            restrict: 'A',//why don't we use 'E'?
+            //Creating Directives that Communicate
+            // responsible for registering DOM listeners as well as updating the DOM
+            link: function (scope, element, attrs) {
+                var sortable = scope.$eval(attrs.accordionSortable);
+                var $accordion = $(element).accordion({
+                    header: "> div > h6"
+                });
+                if (!sortable) return;
+                $accordion.sortable({
+                    axis: "y",
+                    handle: "h6",
+                    stop: function(event, ui) {
+                        // IE doesn't register the blur when sorting
+                        // so trigger focusout handlers to remove .ui-state-focus
+                        ui.item.children("h6").triggerHandler("focusout");
+                    }
+                });
+            }
+        };
+    });
+
+
+    //var $accordion = function() {
+    //    return $("[data-animation='accordion']");
+    //};
+    //var createAccordion = function() {
+    //    $accordion().accordion({
+    //            header: "> div > h6"
+    //        }).sortable({
+    //            axis: "y",
+    //            handle: "h6",
+    //            stop: function(event, ui) {
+    //                // IE doesn't register the blur when sorting
+    //                // so trigger focusout handlers to remove .ui-state-focus
+    //                ui.item.children("h6").triggerHandler("focusout");
+    //            }
+    //        });
+    //};
+
+    myApp.controller('lister', function ($scope) {
+        $scope.steps = [1,2,3];
+        $scope.add = function() {
+            $scope.steps.push(4);
+            //$accordion().delay(1000).accordion("refresh");
+        };
+    });
+
+    //$(function() {
+    //    createAccordion();
+    //});
+})();
